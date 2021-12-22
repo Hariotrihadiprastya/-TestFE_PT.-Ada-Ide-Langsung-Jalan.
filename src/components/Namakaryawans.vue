d96605f143871f65ae4406a26dfc0f3a887ec1b2
@rioedotensaigit config --global user.name @rioedotensaigit config --global user.name @rioedotensai <mersidev.rio@gmail.com>
2021-12-22 12:47:52 +0700

    TesFE Hario

---
+++ src/components/Namakaryawans.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class=" card-title">
+     <h5 class="card-title">{{namakaryawann.nama_karyawan}}</h5>
+    <!-- <p class="card-title ">{{namakaryawann.tanggal_awal}}
+     {{namakaryawann.tanggal_akhir}}</p>
+     <p class="card-text">
+         {{namakaryawann.total_kehadiran}}</p> -->
+     </div>
+     
+     
+</template>
+
+<script>
+export default {
+ name:'Namakaryawans',
+ props: ['namakaryawann']
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <div class="container">
+      <Hero/>   
+       <div class="card text-dark bg-white " style="max-width: 30rem;">
+      <div class="card-header font-weight-bold text-center bg-primary text-white">Faktur Gaji</div>
+      <div class ="card-title">
+     <div class="mb-2" v-for="namakaryawann in namakaryawan" :key="namakaryawann.id">
+      <Namakaryawans :namakaryawann="namakaryawann"/>
+      </div>
+      <p class="text-secondary">01 Januari 2021 - 31 Januari 2021 </p>
+      <p class="text-secondary">Masuk 22 Hari </p>
+      
+    </div>
+  </div>
+    </div>
+
+    </div>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import Namakaryawans from "@/components/Namakaryawans.vue";
+import axios from "axios";
+
+
+
+
+
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+    Hero,
+    Namakaryawans,
+  },
+ data(){
+   return{
+     namakaryawan:[]
+   }
+ },
+  methods: {
+    setnamakaryawan(data) {
+      this.namakaryawan = data;
+    }
+  },
+  mounted() {
+      axios.get( 'https://boss.smartlink.id/salary/inquiry')
+      .then((response)=>this.setnamakaryawan(response.data))
+      .catch((error) => console.log(error)) 
+  },
+};
+</script>
